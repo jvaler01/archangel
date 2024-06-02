3c35f0fd40a5bb0ac52ae277dc044119d658ea39
Jorge Valero Martín <jorgevm13@gmail.com>
2024-06-02 17:15:01 +0200

    feat: refactor exports

---
+++ projects/archangel/src/lib/components/cardComponent/card-index.ts
@@ -0,0 +1,7 @@
+export * from './card/card.component'
+export * from './card-body/card-body.component'
+export * from './card-bottom/card-bottom.component'
+export * from './card-container/card-container.component'
+export * from './card-example/card-example.component'
+export * from './card-header/card-header.component'
+export * from './card-out-text/card-out-text.component'
--- projects/archangel/src/public-api.ts
+++ projects/archangel/src/public-api.ts
@@ -5,12 +5,6 @@
 export * from './lib/archangel.service';
 export * from './lib/archangel.component';
 //CARD
-export * from './lib/components/cardComponent/card/card.component'
-export * from './lib/components/cardComponent/card-body/card-body.component'
-export * from './lib/components/cardComponent/card-bottom/card-bottom.component'
-export * from './lib/components/cardComponent/card-container/card-container.component'
-export * from './lib/components/cardComponent/card-example/card-example.component'
-export * from './lib/components/cardComponent/card-header/card-header.component'
-export * from './lib/components/cardComponent/card-out-text/card-out-text.component'
+export * from './lib/components/cardComponent/card-index'
 //Buttons
 export * from './lib/components/button/standar/standar.component'
